35a95ff986fdfe3911aac80fd4e40ad1010cdc88
Thirawat Eusirwong <torguze@rgmail.com>
2023-03-30 09:30:06 +0700

    Create Nabar - close #2

--- fontend/src/component/Navbar.jsx
+++ fontend/src/component/Navbar.jsx
@@ -1,27 +1,34 @@
 import { Menu } from 'antd';
 
+const { SubMenu } = Menu;
+
 function Navbar() {
-    return (
-        <div className="Navbar">
-            <Menu mode="horizontal">
-                <Menu.Item key="home">
-                    Home
-                </Menu.Item>
+  return (
+    <div className="Navbar">
+      <Menu mode="horizontal">
+        <Menu.Item key="home">Home</Menu.Item>
 
-                <Menu.Item key="shop">
-                    Shop
-                </Menu.Item>
+        <SubMenu key="shop" title="Shop">
+          <Menu.Item key="shop:men">Men</Menu.Item>
+          <Menu.Item key="shop:women">Women</Menu.Item>
+          <Menu.Item key="shop:kids">Kids</Menu.Item>
+        </SubMenu>
 
-                <Menu.Item key="Product">
-                    Add Product
-                </Menu.Item>
+        <SubMenu key="product" title="Add Product">
+          <Menu.Item key="product:addProduct">Add product</Menu.Item>
+          <Menu.Item key="product:updateProduct">Update product</Menu.Item>
+          <Menu.Item key="product:deleteProduct">Delete product</Menu.Item>
+        </SubMenu>
 
-                <Menu.Item key="cart">
-                    Cart
-                </Menu.Item>
-            </Menu>
-        </div>
-    );
+        <Menu.Item key="cart">Cart</Menu.Item>
+
+        <SubMenu key="login" title="Login/Register" style={{float: 'right'}}>
+          <Menu.Item key="product:addProduct">Login</Menu.Item>
+          <Menu.Item key="product:updateProduct">Register</Menu.Item>
+        </SubMenu>
+      </Menu>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
