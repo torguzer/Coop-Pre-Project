f2809d69c4a353759e914dcb76b4a5af6a4acc86
Thirawat Eusirwong <torguze@rgmail.com>
2023-04-03 16:53:14 +0700

    Chang To Use Navigate

--- fontend/src/component/Navbar.jsx
+++ fontend/src/component/Navbar.jsx
@@ -1,55 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Menu } from 'antd';
+import Logo from './Logo/Logo.png';
 
 const { SubMenu } = Menu;
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const goPath = (path) => {
+    navigate(path);
+  };
+
+  
+
   return (
-    <div className="Navbar">
-      <Menu mode="horizontal">
+    <Menu mode="horizontal" style={{ backgroundColor: 'black' }}>
 
-        <Menu.Item key="home">
-          <Link to="/">Home</Link>
+      <img
+        src={Logo}
+        alt="Logo"
+        width={160}
+        height={45}
+        style={{ margintop: '10px' }}
+      />
+
+      <Menu.Item onClick={() => goPath('/')} key="home" style={{ color: '#ffffff' }}>
+        Home
+      </Menu.Item>
+
+      <Menu.Item onClick={() => goPath('/shoppingPage')} key="shopping" style={{ color: '#ffffff' }}>
+        Shopping
+      </Menu.Item>
+
+      <Menu.Item onClick={() => goPath('/cart')} key="cart" style={{ color: '#ffffff' }}>
+        Cart
+      </Menu.Item>
+
+      <SubMenu key="product" title="Product" style={{ color: '#ffffff' }}>
+        <Menu.Item onClick={() => goPath('/productCreate')} key="addProduct">
+          Add Product
         </Menu.Item>
 
-        <Menu.Item key="shoping">
-          <Link to="/shoppingPage">Shopping</Link>
+        <Menu.Item onClick={() => goPath('/')} key="editProduct">
+          Edit Product
         </Menu.Item>
 
-        <Menu.Item key="cart">
-          <Link to="/cart">Cart</Link>
+        <Menu.Item onClick={() => goPath('/')} key="removeProduct">
+          Remove Product
+        </Menu.Item>
+      </SubMenu>
+
+      <SubMenu key="login" title="Login/Signin" style={{ marginLeft: 'auto', color: '#ffffff' }}>
+        <Menu.Item onClick={() => goPath('/')} key="login">
+          Login
         </Menu.Item>
 
-        <SubMenu key="product" title="Product">
-
-          <Menu.Item key="product:addProduct">
-            <Link to="/productCreate">ADD Product</Link>
-          </Menu.Item>
-
-          <Menu.Item key="product:editProduct">
-            <Link to="/">Edit Product</Link>
-          </Menu.Item>
-
-          <Menu.Item key="product:removeProduct">
-            <Link to="/">Remove Product</Link>
-          </Menu.Item>
-        </SubMenu>
-
-        <SubMenu key="login" title="Login/Signin" style={{ float: 'right' }}>
-
-          <Menu.Item key="login">
-            <Link to="/">Login</Link>
-          </Menu.Item>
-
-          <Menu.Item key="Signin">
-            <Link to="/">Signin</Link>
-          </Menu.Item>
-
-        </SubMenu>
-
-      </Menu>
-    </div>
+        <Menu.Item onClick={() => goPath('/')} key="signin">
+          Signin
+        </Menu.Item>
+      </SubMenu>
+    </Menu>
   );
 }
 
